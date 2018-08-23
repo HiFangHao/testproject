4dbd62ec0e28c147a395136ff31c78d7296a6e07
HiFangHao <2535800435@qq.com>
2018-08-23 20:24:18 +0800

    新建了实体类

---
+++ src/main/java/Student.java
@@ -0,0 +1,13 @@
+public class Student {
+
+    private int id;
+    private String username;
+
+    public int getId() {
+        return id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+}
