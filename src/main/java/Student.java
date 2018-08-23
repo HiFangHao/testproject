22cb64c313e71b2d1f71164c08cef7dd72275ffd
HiFangHao <2535800435@qq.com>
2018-08-23 21:01:00 +0800

    添加了实体类

--- src/main/java/Student.java
+++ src/main/java/Student.java
@@ -7,7 +7,16 @@ public class Student {
         return id;
     }
 
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
     public String getUsername() {
         return username;
     }
+
 }
